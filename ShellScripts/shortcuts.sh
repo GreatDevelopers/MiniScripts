e254bac4fc879041b6ca1cba06fe2b5dff337f14
gaganjyot <thegaganx@gmail.com>
2014-11-22 17:36:10 +0530

    Added Shell Scripts

---
+++ ShellScripts/shortcuts.sh
@@ -0,0 +1,16 @@
+alias ls='ls --group-directories-first --time-style=+"%d.%m.%Y %H:%M" --color=auto -F'
+alias ll='ls -l --group-directories-first --time-style=+"%d.%m.%Y %H:%M" --color=auto -F'
+alias la='ls -la --group-directories-first --time-style=+"%d.%m.%Y %H:%M" --color=auto -F'
+alias grep='grep --color=tty -d skip'
+alias cp="cp -i"                          # confirm before overwriting something
+alias df='df -h'                          # human-readable sizes
+alias free='free -m'                      # show sizes in MB
+alias np='nano PKGBUILD'
+alias cd1='cd ..'
+alias cd2='cd ../..'
+alias cd3='cd ../../../'
+alias cd4='cd ../../../../'
+alias cd5='cd ../../../../../'
+alias make='make -j4'
+alias wget='wget -c'
+alias emacs='emacs -nw'
