d278dfc13727f924631de2079c2d306b6a2da3eb
Amritpal Singh <amrit3701@gmail.com>
2015-10-06 17:51:56 +0530

    adding comments

--- PythonOCC-Utils/pythonocc-utils.sh
+++ PythonOCC-Utils/pythonocc-utils.sh
@@ -1,11 +1,15 @@
 #!/bin/sh
 
+# Changing current directory to home directory 
 cd 
 
+# Installing pythonocc-utils 
 git clone https://github.com/tpaviot/pythonocc-utils.git
 
+# Changing home directory to PythonOCC-Utils directory
 cd pythonocc-utils
 
+# Installing PythonOCC-Utils 
 python setup.py install
 
 cp /usr/local/lib/python2.7/dist-packages/* /usr/lib/python2.7/dist-packages/
