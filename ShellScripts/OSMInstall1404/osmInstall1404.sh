6061cad8bc1f01e7927c471abd7820e9620001f1
DaviKaur <skaur910028@gmail.com>
2016-03-02 16:02:29 +0530

    Script to install OSM

--- ShellScripts/OSMInstall1404/osmInstall1404.sh
+++ ShellScripts/OSMInstall1404/osmInstall1404.sh
@@ -93,26 +93,5 @@ sudo a2enconf mod_tile
 sudo service apache2 reload
 # Tuning postgresql #
 sudo vim /etc/postgresql/9.3/main/postgresql.conf
-# Reboot #
+# Reboot your system and run the script two#
 sudo reboot
-# To verify that it displays as 268435456 #
-sudo sysctl kernel.shmmax
-# Loading data into your server #
-udo mkdir /usr/local/share/maps/planet
-sudo chown $USER /usr/local/share/maps/planet
-cd /usr/local/share/maps/planet
-# Importing data into the database #
-osm2pgsql --slim -d gis -C 1500 --number-processes 4 /usr/local/share/maps/planet/*.osm.pbf
-# Testing your tileserver #
-sudo mkdir /var/run/renderd
-sudo chown $USER /var/run/renderd
-sudo -u $USER renderd -f -c /usr/local/etc/renderd.conf
-# Setting it to run automatically #
-sudo cp  ~/src/mod_tile/debian/renderd.init /etc/init.d/renderd
-sudo chmod u+x /etc/init.d/renderd
-sudo vim /etc/init.d/renderd
-# start mapnik #
-sudo /etc/init.d/renderd start
-# stop mapnik #
-sudo /etc/init.d/renderd stop
-sudo ln -s /etc/init.d/renderd /etc/rc2.d/S20renderd
