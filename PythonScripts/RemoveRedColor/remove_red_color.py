f6031227435575b1a741744b6d818c062a1bebc5
satinderpalsingh <satinder.goraya91@gmail.com>
2020-03-20 13:18:11 +0530

    0.0

--- PythonScripts/RemoveRedColor/remove_red_color.py
+++ PythonScripts/RemoveRedColor/remove_red_color.py
@@ -14,6 +14,8 @@ def remove_red_color(input_image):
     upper_red = np.array([10, 150, 255])
     mask1 = cv2.inRange(hsv_img, lower_red, upper_red)
 
+    #cv2.imshow("mask1", mask1)
+    #cv2.waitKey(0)
     # Range for upper red
     lower_red = np.array([155, 10, 60])
     upper_red = np.array([180, 150, 255])
@@ -21,26 +23,34 @@ def remove_red_color(input_image):
 
     # Add both masks for combined effect
     mask = mask1 + mask2
-
+   # cv2.imshow("mask", mask)
+   # cv2.waitKey(0)
+  
     # Cover small area around masked red area
     mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, np.ones((3, 3), np.uint8))
     mask = cv2.morphologyEx(mask, cv2.MORPH_DILATE, np.ones((3, 3), np.uint8))
 
     # To be used as replacement of red color
     white_image = np.zeros(input_img.shape, np.uint8)
-    white_image[:] = (193, 189, 188)
+    white_image[:] = (193, 189, 188) #colour near to sheet 
 
     # Remove part of white image corresponding to which there is no red color in
     # input image
     res1 = cv2.bitwise_and(white_image, white_image, mask=mask)
-
+    #cv2.imshow("res1", res1)
+    #cv2.waitKey(0)
+  
     # Remove part of input image containing red color
     mask = cv2.bitwise_not(mask)
     res2 = cv2.bitwise_or(input_img, input_img, mask=mask)
-
+    #cv2.imshow("res2", res2)
+    #cv2.waitKey(0)
+  
     # Add result1 and result2
     res = cv2.bitwise_or(res1, res2)
-
+    #cv2.imshow("res", res)
+    #cv2.waitKey(0)
+  
     # Save output file
     res = res[:, :, 2]
     output_file = input_image.split("/")[-1].split(".")[0] + "_output.png"
