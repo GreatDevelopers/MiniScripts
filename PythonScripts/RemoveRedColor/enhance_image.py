edf61ca9a50f11e448f359f20121840ef7a86c2c
SurajDadral <dadralj18@gmail.com>
2020-03-26 02:45:36 +0530

    Apply Gaussian blur to smooth text in image

--- PythonScripts/RemoveRedColor/enhance_image.py
+++ PythonScripts/RemoveRedColor/enhance_image.py
@@ -16,7 +16,7 @@ def enhance_image(input_image):
     # BGR value
     off_white_img = np.where(
         morphology_img[:, :] == [255, 255, 255],
-        np.array([200, 200, 200], dtype=np.uint8),
+        np.array([241, 241, 249], dtype=np.uint8),
         morphology_img,
     )
     off_white_img.dtype = np.uint8
@@ -33,8 +33,14 @@ def enhance_image(input_image):
     # hsv_img[..., 2] = hsv_img[..., 2] * 0.6
     # less_bright_img = cv2.cvtColor(hsv_img, cv2.COLOR_HSV2BGR)
 
+    # Remove noise from image i.e. small black dots here and there in image
+    denoised_img = cv2.fastNlMeansDenoising(less_black_img, 11, 31, 9)
+
+    # Apply Gaussian blur to image to make text smooth
+    blur_img = cv2.GaussianBlur(denoised_img,(3,3),0)
+
     # Save output file
-    cv2.imwrite(input_image, less_black_img)
+    cv2.imwrite(input_image, blur_img)
 
     return input_image
 
